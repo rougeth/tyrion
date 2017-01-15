87454869b65b26764a324c8c5d44ae3bb3e89109
Marco Rougeth <marco@rougeth.com>
2017-01-15 17:12:00 -0200

    Add 404 page

--- src/tyrion/settings.py
+++ src/tyrion/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 try:
