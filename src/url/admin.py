00a58e310f216409f4c55e8072779077da2a5cd9
Marco Rougeth <marco@rougeth.com>
2017-01-19 23:19:07 -0200

    Count how many clicks an url gets

--- src/url/admin.py
+++ src/url/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import URL
+from .models import URL, Click
 
 
 @admin.register(URL)
 class URLAdmin(admin.ModelAdmin):
-    list_display = ['short', 'url', 'created', 'updated']
+    list_display = ['short', 'url', 'created', 'updated', 'clicks']
+
+    # Maybe there's a better way to get an annotate field
+    def clicks(self, obj):
+        return obj.clicks
+    clicks.admin_order_field = 'clicks'
+
+
+@admin.register(Click)
+class ClickAdmin(admin.ModelAdmin):
+    list_display = ['url', 'ip', 'date']
--- src/url/views.py
+++ src/url/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.views.defaults import page_not_found
 
+from ipware.ip import get_real_ip
+
 from pybaco import Baco, base62
 
-from .models import URL
+from .models import URL, Click
 
 
 def url_redirect(request, short):
@@ -13,5 +15,9 @@ def url_redirect(request, short):
         url = URL.objects.get(id=id)
     except URL.DoesNotExist as e:
         return page_not_found(request, e, 'url/404.html')
+
+    # Get a more precise request datetime
+    ip = get_real_ip(request) or '0.0.0.0'
+    Click.objects.create(url=url, ip=ip)
 
     return HttpResponseRedirect(url.url)
